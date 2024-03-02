4033d7415e4ec2695315e407afa8b37cfd26ccb0
Sayul <sayulramirez@gmail.com>
2024-03-02 10:14:56 -0600

    Sobreescribiendo el método handleMethodArgumentNotValid de la clase ResponseEntityExceptionHandler para poder personalizar mejor la respuesta de error de la validación del controler con la etiqueta @Valid y la clase User

--- src/main/java/com/challeng/foro/controllers/RestResponseEntityEcxeptionHandler.java
+++ src/main/java/com/challeng/foro/controllers/RestResponseEntityEcxeptionHandler.java
@@ -1,14 +1,19 @@
 package com.challeng.foro.controllers;
 
 import com.challeng.foro.exceptions.BadRequestException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -28,4 +33,28 @@ public class RestResponseEntityEcxeptionHandler
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String, Object> errors = new HashMap<>();
+
+        errors.put("timestamp: ", LocalDateTime.now());
+
+        ex.getBindingResult().getAllErrors().forEach(
+                (error) -> {
+
+                    String fieldName = ((FieldError) error).getField();
+                    String message = error.getDefaultMessage();
+
+                    errors.put(fieldName, message);
+                }
+        );
+
+        errors.put("Error: ", status.toString());
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
